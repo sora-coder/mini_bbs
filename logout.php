ceacabe252ec7119bbf0d03269f90465318d66b0
sora-coder <rqqgx271@yahoo.co.jp>
2021-02-28 12:10:50 +0900

    ini_setをini_getに変更

--- logout.php
+++ logout.php
@@ -2,7 +2,7 @@
     session_start();
 
     $_SESSION = array();//セッション変数を全て解除する
-    if(ini_set('session.use_cookies')){//sessionにcookieを使うかどうか設定するファイル
+    if(ini_get('session.use_cookies')){//sessionにcookieを使うかどうか設定するファイル
         $params = session_get_cookie_params();//sessionのパラーメータを入れる
         setcookie(session_name() . '', time() - 42000, $params['path'], $params['domain'], $params['secure'], $params['httponly']);//cookieの有効期限を切る
     }
